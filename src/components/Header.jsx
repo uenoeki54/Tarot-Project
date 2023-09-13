18754eeceba61db156a814f3865e75dc9ffa6cc6
uenoeki54 <uenoeki54ban@gmail.com>
2023-09-13 15:21:15 +0200

    Premier commit du contenu

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import banner from '../assets/biglogo.png';
+
+function Header() {
+  return (
+    <header>
+      <img className="banner" src={banner} alt="banner tarot project"></img>
+      <nav>
+        <Link to="/"> Accueil </Link>
+        <Link to="/apropos"> A propos </Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
