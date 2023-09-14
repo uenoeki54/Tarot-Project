7f93fac55dff33168595ca46da6bb44efac410c7
uenoeki54 <uenoeki54ban@gmail.com>
2023-09-14 19:06:35 +0200

    pages dynamiques

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -4,49 +4,89 @@ import Collapse from './Collapse/Collapse';
 function Aside() {
   return (
     <div className="menu">
-      <Link to="/"> home </Link>
-      <Collapse title="Arcana" size="small">
-        <ul>
-          <li>
-            <Link to="./Card/00">0</Link>
-          </li>
+      <Link to="/">Back to Intromission</Link>
+      <Collapse title="Major Arcana" size="small">
+        <li>
+          <Link to="./Card/01">I</Link>
+        </li>
 
-          <li>
-            <Link to="./Card/01">I</Link>
-          </li>
+        <li>
+          <Link to="./Card/02">II</Link>
+        </li>
 
-          <li>
-            <Link to="./Card/02">II</Link>
-          </li>
+        <li>
+          <Link to="./Card/03">III</Link>
+        </li>
+        <li>
+          <Link to="./Card/04">IV</Link>
+        </li>
+        <li>
+          <Link to="./Card/05">VI</Link>
+        </li>
+        <li>
+          <Link to="./Card/06">VII</Link>
+        </li>
+        <li>
+          <Link to="./Card/07">VIII</Link>
+        </li>
+        <li>
+          <Link to="./Card/08">Ⅸ</Link>
+        </li>
+        <li>
+          <Link to="./Card/09">X</Link>
+        </li>
+        <li>
+          <Link to="./Card/10">XI</Link>
+        </li>
+        <li>
+          <Link to="./Card/11">XII</Link>
+        </li>
+      </Collapse>
+      <Collapse title="Air- Swords" size="small">
+        <li>
+          <Link to="./Card/11">King/Knight</Link>
+        </li>
 
-          <li>
-            <Link to="./Card/03">III</Link>
-          </li>
-          <li>
-            <Link to="./Card/04">IV</Link>
-          </li>
-          <li>
-            <Link to="./Card/05">VI</Link>
-          </li>
-          <li>
-            <Link to="./Card/06">VII</Link>
-          </li>
-          <li>
-            <Link to="./Card/07">VIII</Link>
-          </li>
-          <li>
-            <Link to="./Card/08">Ⅸ</Link>
-          </li>
-          <li>
-            <Link to="./Card/09">X</Link>
-          </li>
-          <li>
-            <Link to="./Card/10">XI</Link>
-          </li>
-          <li>
-            <Link to="./Card/11">XII</Link>
-          </li>
-        </ul>
+        <li>
+          <Link to="./Card/12">Queen</Link>
+        </li>
+
+        <li>
+          <Link to="./Card/13">Prince/Knight</Link>
+        </li>
+        <li>
+          <Link to="./Card/14">Princess/Page</Link>
+        </li>
+        <li>
+          <Link to="./Card/101">Ace</Link>
+        </li>
+        <li>
+          <Link to="./Card/102">Two</Link>
+        </li>
+        <li>
+          <Link to="./Card/103">Three</Link>
+        </li>
+        <li>
+          <Link to="./Card/104">Four</Link>
+        </li>
+        <li>
+          <Link to="./Card/105">Five</Link>
+        </li>
+        <li>
+          <Link to="./Card/106">Six</Link>
+        </li>
+        <li>
+          <Link to="./Card/107">Seven</Link>
+        </li>
+        <li>
+          <Link to="./Card/108">Eight</Link>
+        </li>
+        <li>
+          <Link to="./Card/109">Nine</Link>
+        </li>
+        <li>
+          <Link to="./Card/110">Tenth</Link>
+        </li>
       </Collapse>
     </div>
   );
--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,56 +1,19 @@
 import toteninsel from '../assets/toteninsel400.jpg';
+import { useParams } from 'react-router-dom';
+import cards from '../assets/cards.json';
+
 function Card() {
+  const { cardId } = useParams();
+  let card = cards.filter((cards) => cards.id === cardId);
+  console.log(card[0].id);
+  console.log(card[0].essay);
+
   return (
     <div className="main">
-      <img src={toteninsel} className="main-illustration"></img>
+      <img src={card[0].cover} className="main-illustration"></img>
       <div className="text">
-        <h1>Caret generique</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry s standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.<br></br>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry s standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of
-          the printing and typesetting industry. Lorem Ipsum has been the
-          industry s standard dummy text ever since the 1500s, when an unknown
-          printer took a galley of type and scrambled it to make a type specimen
-          book. It has survived not only five centuries, but also the leap into
-          electronic typesetting, remaining essentially unchanged. It was
-          popularised in the 1960s with the release of Letraset sheets
-          containing Lorem Ipsum passages, and more recently with desktop
-          publishing software like Aldus PageMaker including versions of Lorem
-          Ipsum. Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry. Lorem Ipsum has been the industry s standard
-          dummy text ever since the 1500s, when an unknown printer took a galley
-          of type and scrambled it to make a type specimen book. It has survived
-          not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in
-          the 1960s with the release of Letraset sheets containing Lorem Ipsum
-          passages, and more recently with desktop publishing software like
-          Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is
-          simply dummy text of the printing and typesetting industry. Lorem
-          Ipsum has been the industry s standard dummy text ever since the
-          1500s, when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book. It has survived not only five centuries,
-          but also the leap into electronic typesetting, remaining essentially
-          unchanged. It was popularised in the 1960s with the release of
-          Letraset sheets containing Lorem Ipsum passages, and more recently
-          with desktop publishing software like Aldus PageMaker including
-          versions of Lorem Ipsum.
-        </p>
+        <h1>{card[0].title}</h1>
+        <p>{card[0].essay}</p>
       </div>
     </div>
   );
