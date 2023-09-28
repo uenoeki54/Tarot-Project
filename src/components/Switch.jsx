53121d53c2f1d04f6a794273fdb0855dcd9d5757
uenoeki54 <uenoeki54ban@gmail.com>
2023-09-28 16:22:29 +0200

    site remarche, corrigé la syntaxe dans Switch.jsx

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { ThemeContext } from '../index.js';
 function Switch() {
-  React.useContext({ ThemeContext });
-  return (
-    <button className="switch">
-      <h1>{value} + Theme</h1>
-    </button>
-  );
+  const value = React.useContext(ThemeContext);
+  return <button className="switch">{<h1>{value} + Theme</h1>}</button>;
 }
 
 export default Switch;
